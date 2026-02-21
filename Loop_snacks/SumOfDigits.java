eeb65763aaff2944045d4efe99a469b714cdcd3c
amoduenechojo <amoduchojo27@gmail.com>
2026-02-21 13:48:02 +0100

    <<Loop snacks>>

---
+++ Loop_snacks/SumOfDigits.java
@@ -0,0 +1,17 @@
+public class SumOfDigits{
+    public static void main(String [] args){
+
+//    System.out.println("Enter ")   
+
+   int sum = 0;
+//   int numeric = 0;
+
+
+   for(int number = 0; number <= 1000; number++){
+    sum+=number;
+    }
+    System.out.println(sum);
+
+}
+    }
+
